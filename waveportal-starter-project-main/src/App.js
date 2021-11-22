c7f57d7f3859009fe22b05ca56295ea601bee0b5
Rahul Patni <rahul20patni@gmail.com>
2021-11-22 12:49:16 -0500

    adding waves

--- waveportal-starter-project-main/src/App.js
+++ waveportal-starter-project-main/src/App.js
@@ -1,9 +1,12 @@
-import * as React from "react";
-// import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import "./App.css";
+import abi from "./utils/WavePortal.json";
 
 export default function App() {
   const [currentAccount, setCurrentAccount] = useState("");
+  const contractAddress = "0x34d939D6c875c108Be60e5b7bC90dEe9b7f8e633";
+  const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -24,6 +27,41 @@ export default function App() {
         setCurrentAccount(account);
       } else {
         console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+         * Execute the actual wave from your smart contract
+         */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
       console.log(error);
@@ -54,16 +92,14 @@ export default function App() {
     checkIfWalletIsConnected();
   }, []);
 
-  const wave = () => {};
-
   return (
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">👋 Hey there!</div>
 
         <div className="bio">
-          I am farza and I worked on self-driving cars so that's pretty cool
-          right? Connect your Ethereum wallet and wave at me!
+          I am Patni and I'm a full tech crypto degen. Connect your Ethereum
+          wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
@@ -74,6 +110,10 @@ export default function App() {
           <button className="waveButton" onClick={connectWallet}>
             Connect Wallet
           </button>
+        )}
+
+        {currentAccount && (
+          <div className="walletConnected">Wallet Connected!</div>
         )}
       </div>
     </div>
