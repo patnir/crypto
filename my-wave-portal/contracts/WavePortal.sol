6136a94d94a2aec254851918f68d3722e8bba180
Rahul Patni <rahul20patni@gmail.com>
2021-11-22 16:09:33 -0500

    realtime updates

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    mapping(address => uint256) public lastWavedAt;
 
     event NewWave(address indexed from, uint256 timestamp, string message);
     struct Wave {
@@ -13,17 +14,50 @@ contract WavePortal {
         string message; // The message the user sent.
         uint256 timestamp; // The timestamp when the user waved.
     }
+    uint256 private seed;
 
     Wave[] waves;
 
-    constructor() {
-        console.log("I AM SMART CONTRACT. POG.");
+    constructor() payable {
+        console.log("We have been constructed!");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
+        /*
+         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+         */
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+
         waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * Generate a new seed for the next user that sends a wave
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        if (seed <= 15) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than they contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
 
         emit NewWave(msg.sender, block.timestamp, _message);
     }
--- my-wave-portal/scripts/deploy.js
+++ my-wave-portal/scripts/deploy.js
@@ -5,11 +5,14 @@ const main = async () => {
   console.log("Deploying contracts with accounts: ", deployer.address);
   console.log("Account balance: ", accountBalance.toString());
 
-  const Token = await hre.ethers.getContractFactory("WavePortal");
-  const portal = await Token.deploy();
-  await portal.deployed();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.002"),
+  });
 
-  console.log("Wave portal address: ", portal.address);
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
 };
 
 const runMain = async () => {
